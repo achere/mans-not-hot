52c2ab0e1287febc97566c93763539d776050704
achere <chethelesser@gmail.com>
2018-02-19 21:46:40 +0300

    Adding missing essential sounds and a basic script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hey');     
+console.log('Man can never be hot!');
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.play();
+});
