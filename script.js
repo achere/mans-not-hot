fddfb02ab976e0e9ef703f3d00aff969070411da
achere <chethelesser@gmail.com>
2018-02-24 22:19:39 +0300

    Updated audio files, changed assigned keys, created .playing class and made it being added and removed to divs when pressing corresponding key

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-console.log('Man can never be hot!');
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) return;
-  audio.play();
+document.addEventListener("DOMContentLoaded", function(e) {
+  console.log('Man can never be hot!');
+
+  window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+  });
+
+  const removeTransition = function(e) {
+    if (e.propertyName != 'transform') return;
+    this.classList.remove('playing');
+    console.log(this.classList);
+    this.className='key';
+  };
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  console.log(keys);
+
 });
